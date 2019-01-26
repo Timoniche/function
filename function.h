bc01ddad62b966012972541097f94a7bfdb52f19
Dmitrii Dulaev <dmitriidulaev@gmail.com>
2019-01-26 15:17:20 +0300

    r 1.0

--- function.h
+++ function.h
@@ -26,7 +26,7 @@ private:
     template<typename fT>
     class f_holder : public f_holder_base {
     public:
-        f_holder(fT f) : f_holder_base(), _fun(std::move(f)) {} //NOLINT
+        f_holder(fT f) : f_holder_base(), _fun(f) {} //NOLINT
 
         retT call(argT ... args) override {
             return _fun(std::forward<argT>(args) ...);
@@ -37,7 +37,7 @@ private:
     };
 //________________________________________________________________
 private:
-    static const size_t GAP_BYTE_SIZE = 16;
+    static const size_t GAP_BYTE_SIZE = 32;
 
     union any_data {
         call_t _call;
@@ -73,25 +73,30 @@ public:
         }
     }
 
-    function(function &&other) noexcept {
-        is_small = false;
-        swap(other);
+    function(function &&other) noexcept : is_small(other.is_small) {
+        if (is_small) {
+            std::copy(other._data.buffer,
+                      other._data.buffer + GAP_BYTE_SIZE, _data.buffer);
+        } else {
+            new(&_data._call) call_t(std::move(other._data._call));
+            new(&other._data._call) call_t(nullptr);
+        }
     }
 
     template<typename fT>
     function(fT f) { //NOLINT
         if /*constexpr*/ (sizeof(f) <= GAP_BYTE_SIZE) {
             is_small = true;
-            new(&_data.buffer) f_holder<fT>(std::move(f));
+            new(&_data.buffer) f_holder<fT>(f);
         } else {
             is_small = false;
-            new(&_data._call) std::unique_ptr<f_holder<fT>>(new f_holder<fT>(std::move(f)));
+            new(&_data._call) std::unique_ptr<f_holder<fT>>(new f_holder<fT>(f));
         }
     }
 
     ~function() {
         if (is_small) {
-            ((f_holder_base *) (_data.buffer))->~f_holder_base();
+            (reinterpret_cast<f_holder_base *>(_data.buffer))->~f_holder_base();
         }
     }
 
@@ -105,7 +110,7 @@ public:
         if (!is_small) {
             _data._call.reset();
         }
-        this->swap(other);
+        swap(other);
         return *this;
     }
 
@@ -115,7 +120,7 @@ public:
                 std::swap(_data.buffer[i], rhs._data.buffer[i]);
             }
         } else if (!is_small && !rhs.is_small) {
-            std::swap(_data._call, rhs._data._call);
+            _data._call.swap(rhs._data._call);
         } else if (is_small) {
             swap_small_big(_data, rhs._data);
         } else {
@@ -130,10 +135,10 @@ public:
 
     retT operator()(argT ... args) const {
         if (is_small) {
-            return ((f_holder_base *) (_data.buffer))->call(
+            return (reinterpret_cast<f_holder_base *>(const_cast<unsigned char *>(_data.buffer)))->call(
                     std::forward<argT>(args)...);
         } else {
-            _data._call->call(args ...);
+            _data._call->call(std::forward<argT>(args) ...);
         }
     }
 //_______________________________________________________________
@@ -141,7 +146,7 @@ private:
     void swap_small_big(any_data &small, any_data &big) {
         unsigned char tmp[GAP_BYTE_SIZE];
         std::copy(small.buffer, small.buffer + GAP_BYTE_SIZE, tmp);
-        new(&small._call) call_t(std::move(big._call));
+        new(&small._call) call_t(big._call.get());
         big._call.reset();
         std::copy(tmp, tmp + GAP_BYTE_SIZE, big.buffer);
     }
